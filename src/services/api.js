2c5cc05fc6d9423d27e080148aa1f3ac0b896ee4
Amrish-pathak <gg620093@gmail.com>
2026-05-20 18:26:23 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://YOUR_BACKEND_URL",
+  baseURL: "https://tgautomactiontool-backend.onrender.com/",
 });
 
 export default api;
