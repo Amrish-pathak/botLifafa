3a7f313f9ff0f0bb2bb2264ff27d11ad225c17de
Amrish-pathak <gg620093@gmail.com>
2026-09-23 16:23:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    target: "es2018",
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          telegram: ["@twa-dev/sdk"],
+        },
+      },
+    },
+  },
 });
