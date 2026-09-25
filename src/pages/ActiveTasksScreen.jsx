d82563fceb3be9b45a8d6a8177dbdfe55e0c5f9a
Amrish-pathak <gg620093@gmail.com>
2026-09-25 06:37:39 +0530

    Update ActiveTasksScreen.jsx

--- src/pages/ActiveTasksScreen.jsx
+++ src/pages/ActiveTasksScreen.jsx
@@ -18,7 +18,7 @@ export default function ActiveTasksScreen() {
 
   useEffect(() => {
     api
-      .get("/botlifafa/active-tasks")
+      .get("/api/bottasks/active-tasks")
       .then((res) => setData(res.data))
       .catch((err) => console.error("active-tasks fetch error:", err))
       .finally(() => setLoading(false));
